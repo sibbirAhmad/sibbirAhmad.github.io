86017f85669f051952d2ddf6c03a80c006f44342
sibbirahmad <sibbirahmad.01@gmail.com>
2022-11-27 14:41:25 +0600

    Updating app update

--- apps/ababil/data/data.js
+++ apps/ababil/data/data.js
@@ -1,8 +1,36 @@
-async function fetchData(){
-    const response = await fetch("/../../news.json");
+async function fetchData(url){
+    const response = await fetch(url);
     const data = response.json();
     return data;
 }
-fetchData().then(data=>{
-    document.write(JSON.stringify(data));
+var p = getURLParams();
+var param = p.p;
+const dataJson = {};
+fetchData("data.json").then(data=>{
+    if(param!=data.updateCode){
+        console.log("Need to aupdate app");
+        dataJson.appData = data;
+    }else{
+        console.log("App is Up to date");
+       
+    }
+    console.log(data.appUpdate.versionCode);
 });
+fetchData("/../../news.json").then(data=>{
+    dataJson.news = data;
+    document.write(JSON.stringify(dataJson));
+});
+function getURLParams() {
+    var params = new Object();
+    var query = window.location.search.substring(1);
+    var pairs = query.split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        var pos = pairs[i].indexOf('=');
+        if (pos > 0) {
+            var key = pairs[i].substring(0, pos);
+            var val = pairs[i].substring(pos + 1);
+            params[key] = val;
+        }
+    }
+    return params;
+}
